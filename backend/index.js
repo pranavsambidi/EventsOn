fd4cf96536babe5be16de764aed0b93bc4215672
pranavsambidi <pranavreddy0709@gmail.com>
2025-04-12 18:16:25 +0530

    chnaged origin in index.js backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT= process.env.PORT || 8080;
 
 app.use(bodyparser.json());
 app.use(cors({
-    origin: 'https://events-on.vercel.app/',
+    origin: 'https://events-on.vercel.app',
     methods: 'GET,POST,PUT,DELETE', 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
